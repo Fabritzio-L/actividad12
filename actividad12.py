3d8c3009af5242f1b1b3b9c5a68f3c088a3532bc
fabri <fabritzioalejandrol@gmail.com>
2025-08-01 09:40:17 -0600

    Pedir datos al usuario

---
+++ actividad12.py
@@ -0,0 +1,12 @@
+estudiantes={}
+try:
+    cantidad_estudiantes= int(input("¿Cuantos estudiantes desea ingresar: "))
+    notas={}
+    for i in range(cantidad_estudiantes):
+        carnet= input("Ingrese carnet del estudiante: ")
+        nombre= input("Ingrese nombre del estudiante: ")
+        cantidad_notas= int(input("¿Cuantas notas desea ingresar?: "))
+        for j in range(cantidad_notas):
+            notas= int(input(("Ingrese la nota: "))
+
+
