a3a3290f48a245592d0cbc569e825d7948407759
fabri <fabritzioalejandrol@gmail.com>
2025-08-01 17:19:02 -0600

    Error si ingresa valores validos en cantidad de notas y estudiantes

--- actividad12.py
+++ actividad12.py
@@ -1,12 +1,32 @@
 estudiantes={}
 try:
     cantidad_estudiantes= int(input("¿Cuantos estudiantes desea ingresar: "))
-    notas={}
+except ValueError:
+        print("Error: ingrese valores validos.")
+else:
     for i in range(cantidad_estudiantes):
-        carnet= input("Ingrese carnet del estudiante: ")
         nombre= input("Ingrese nombre del estudiante: ")
-        cantidad_notas= int(input("¿Cuantas notas desea ingresar?: "))
-        for j in range(cantidad_notas):
-            notas= int(input(("Ingrese la nota: "))
-
-
+        notas=[]
+        try:
+            cantidad_notas= int(input("¿Cuantas notas desea ingresar?: "))
+        except ValueError:
+            print("Error: ingrese valores validos.")
+            for j in range(cantidad_notas):
+                nota= int(input("Ingrese la nota: "))
+                notas.append(nota)
+                for nota in notas:
+                    promedio = sum(nota)/len(nota)
+    except ValueError:
+        print("Error: ingrese valores validos.")
+    except ZeroDivisionError:
+        print("Error: no se puede calcular promedio sin notas ingresadas.")
+    except TypeError:
+        print("Error: No se pueden combinar diferentes tipos de datos")
+    except Exception as e:
+        print("Se produjo un error inesperado:", e)
+    else:
+        for estudiante in estudiantes:
+            print(estudiante)
+            print(promedio)
+    finally:
+        print("Fin del proceso")
